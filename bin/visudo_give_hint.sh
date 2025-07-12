1e94f5cf3c95b8f2ee3f110d3dfad5d70137b302
John de Graaff <john@de-graaff.net>
2025-07-12 13:42:52 +0200

    some

---
+++ bin/visudo_give_hint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+echo
+#
+cat <<HERE
+Defaults editor=/usr/bin/vim
+# username ALL=(ALL) NOPASSWD:ALL
+%sudo ALL=(ALL) NOPASSWD:ALL
+HERE
+#
+echo
+#
+read -p "press enter to start 'visudo' (but first copy above text) ... "
+#
+visudo
+#
+#-eof
+
