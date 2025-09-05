a3f8d9e089e835a522a786625c57a1604899c928
John de Graaff <john@de-graaff.net>
2025-09-05 11:45:42 +0200

    some

--- bin/git-config-jdg.sh
+++ bin/git-config-jdg.sh
@@ -8,7 +8,7 @@ git config --global alias.ss "status -s"        # short status
 git config --global alias.logg "log --oneline --decorate --graph --all --pretty=format:'%C(auto)%h %aD %d %s'"
 git config --global alias.logn "log --oneline --decorate --graph --all --pretty=format:'%h - %ae - %ad : %s' --numstat"
 # git log --date=iso --pretty=format:'%ad, %h, %s' | awk '$0 >= "2025-05-23" && $0 <= "2025-10-01"'
-git config --global alias.logd "git log --date=iso --pretty=format:'%ad, %h, %s'"
+git config --global alias.logd "log --date=iso --pretty=format:'%ad, %h, %s'"
 git config --global pull.rebase false
 #
 #-EOF
