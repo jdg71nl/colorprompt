2b5ebd042ba996945de7683e8273038fafb5c2d2
John de Graaff <john@de-graaff.net>
2025-09-05 11:47:12 +0200

    some

--- bin/git_log_date_between_sum.sh
+++ bin/git_log_date_between_sum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-./git_log_date_between.sh | egrep '{' | perl -pe 's/^.*{(\d+)}.*$/$1/' | awk '{s+=$1} END {print s}' 
+git_log_date_between.sh | egrep '{' | perl -pe 's/^.*{(\d+)}.*$/$1/' | awk '{s+=$1} END {print s}' 
 set +x
 
 #-eof
