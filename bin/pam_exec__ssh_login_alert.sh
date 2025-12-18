7ce5242ea58d1c032483cfdc0e237eaa95362f76
John de Graaff <john@de-graaff.net>
2025-12-18 18:03:30 +0100

    some

--- bin/pam_exec__ssh_login_alert.sh
+++ bin/pam_exec__ssh_login_alert.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 #= /usr/local/bin/pam_exec__ssh_login_alert.sh
 
-# d251217-jdg: idea from Gemini:
+# d251217-jdg: idea from Gemini
 
-# install packages:
-# > sudo apt update && sudo apt install postfix mailutils geoip-bin -y
-
-# > vi /etc/pam.d/sshd
+# - - - - - - = = = - - - - - - . 
+# install:
+# (1)
+# > sudo cp -av ~/colorprompt/bin/pam_exec__ssh_login_alert.sh /usr/local/bin/pam_exec__ssh_login_alert.sh
+# (2)
+# > sudo vi /usr/local/bin/pam_exec__ssh_login_alert.sh
+# (edit ADMIN_EMAIL_ADDRESS)
+# (2)
+# > sudo vi /etc/pam.d/sshd
 # (add at bottom:)
 # session optional pam_exec.so /usr/local/bin/pam_exec__ssh_login_alert.sh
 # (no need to reload PAM or anything)
+# (3)
+# > sudo apt update && sudo apt install postfix mailutils geoip-bin -y
+# - - - - - - = = = - - - - - - . 
 
 ADMIN_EMAIL_ADDRESS="name@host.dom"
 
@@ -20,7 +28,8 @@ if [ "$PAM_TYPE" != "close_session" ]; then
 
     GEOIP=$(geoiplookup $PAM_RHOST)
 
-    RDNS=$(host $PAM_RHOST | head -n1 | awk -F "domain name pointer " '{print $2}')
+    #RDNS=$(host $PAM_RHOST | head -n1 | awk -F "domain name pointer " '{print $2}')
+    RDNS=$(host $PAM_RHOST | head -n1 | perl -pe 's/^.*domain name pointer (.*?)\.$/$1/')
 
     BODY="User $PAM_USER logged in from $PAM_RHOST on $(date), GeoIP='$GEOIP', Reverse-DNS='$RDNS' "
 
