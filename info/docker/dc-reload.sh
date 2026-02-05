09a61fcfa47293a16537324f44d18a9dce83f22c
John de Graaff <john@de-graaff.net>
2026-02-05 14:26:53 +0100

    some

---
+++ info/docker/dc-reload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+BASENAME=`basename $0`
+SCRIPT=`realpath $0`             # -s, --strip, --no-symlinks : don't expand symlinks
+SCRIPT_PATH=`dirname $SCRIPT`
+# echo "SCRIPT='$SCRIPT"            # SCRIPT='/Users/jdg/dev/mern-template/deploy/docker/dc-mongo-52-up.sh
+# echo "SCRIPT_PATH='$SCRIPT_PATH"  # SCRIPT_PATH='/Users/jdg/dev/mern-template/deploy/docker
+#cd $SCRIPT_PATH
+#
+#YAML_FILE="docker-compose.yaml"
+#YAML_PATH="$SCRIPT_PATH/$YAML_FILE"
+
+#
+docker exec smokeping pkill -f -HUP '/usr/bin/perl /usr/s?bin/smokeping(_cgi)?'
+
+#-eof
