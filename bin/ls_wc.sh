d6485f8c6116a2a6a18bc72bfca1618afd5e8ea7
John de Graaff <john@de-graaff.net>
2026-04-03 22:06:48 +0200

    some

---
+++ bin/ls_wc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# ^^^ better than: #!/bin/bash
+#= ls_wc.sh | updated: d260403
+# (c) John@de-Graaff.net
+# - - - - - - = = = - - - - - - . 
+
+ls -1 "$@" | wc
+
+# - - - - - - = = = - - - - - - . 
+#-eof
+
