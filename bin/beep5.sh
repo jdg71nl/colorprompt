8b328ae648c19a207bcddb30a1690232f0d2549d
John de Graaff <john@de-graaff.net>
2026-04-04 11:04:11 +0200

    some

---
+++ bin/beep5.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# - - - - - - = = = - - - - - - . 
+#printf "\a"; sleep 0.2; printf "\a"; sleep 0.2; printf "\a";sleep 0.2; printf "\a";sleep 0.2; printf "\a";
+#
+for ((n=1; n <= 5; n++)); do printf "\a"; sleep 0.2; done
+# - - - - - - = = = - - - - - - . 
+#-eof
+
