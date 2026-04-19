f0d2a6388d89724665e26cd26f801e330c51c36d
John de Graaff <john@de-graaff.net>
2026-04-19 10:41:13 +0200

    some

--- bin/get_docker.sh
+++ bin/get_docker.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 #
 # found on: https://forums.raspberrypi.com/viewtopic.php?t=320769
+# top of file has info: https://get.docker.com
+# https://docs.docker.com/engine/install/
 #
+cat <<HERE
+
+# either run unattended:
+
 curl -sSL https://get.docker.com | bash
+
+# or save to file, and inspect/change/run manually:
+
+curl -fsSL https://get.docker.com -o install-docker.sh
+sh install-docker.sh --dry-run
+sudo sh install-docker.sh
+
+HERE
 #
 #-eof
