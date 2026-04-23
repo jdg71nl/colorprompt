c9aa2d0243c53bba20364faa3269e3789448990a
John de Graaff <john@de-graaff.net>
2026-04-23 21:51:18 +0200

    some

--- bin/date_print_unix_epoch.sh
+++ bin/date_print_unix_epoch.sh
@@ -3,5 +3,13 @@ DATE_EPOCH=$(date +%s)
 echo "# > DATE_EPOCH=\$(date +%s)"
 echo "# > echo \$DATE_EPOCH"
 echo $DATE_EPOCH
+echo
+echo "# additional info:"
+cat <<HERE
+> date +%s.%N
+    %s: Seconds since the Unix Epoch (Jan 1, 1970).
+    %N: Nanoseconds.
+
+HERE
 #-eof
 
