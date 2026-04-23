000e200e5ec98455a451a1329eb77180739a6f2b
John de Graaff <john@de-graaff.net>
2026-04-23 21:27:58 +0200

    some

---
+++ bin/linux_show_uptime_seconds_since_kernel_start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# - - - - - - = = = - - - - - - . 
+set -x
+cat /proc/uptime
+echo "# NOTE: that the 1st column of dmesg is also this uptime-seconds"
+# - - - - - - = = = - - - - - - . 
+#-eof
+
