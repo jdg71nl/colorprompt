28a6adf986f428a24e1ecede84d8f6de19297b3e
John de Graaff <john@de-graaff.net>
2026-05-25 21:49:48 +0200

    some

--- bin/create_bash_profile_rc.sh
+++ bin/create_bash_profile_rc.sh
@@ -2,13 +2,15 @@
 #= create_bash_profile_rc.sh 
 
 cd
-cat <<HERE >> .bash_profile
+if ! grep -q bashrc .bash_profilea; then
+  cat <<HERE >> .bash_profile
 #= .bash_profile
 if [ -f ~/.bashrc ]; then
   . ~/.bashrc
 fi
 #-eof
 HERE
+fi
 
 # - - - - - - = = = - - - - - - . 
 #-eof
