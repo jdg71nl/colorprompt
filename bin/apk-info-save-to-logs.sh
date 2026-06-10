4cf90401f23b467ac5c4c3e4447c4d547cb2b33b
John de Graaff <john@de-graaff.net>
2026-06-10 20:31:34 +0200

    some

--- bin/apk-info-save-to-logs.sh
+++ bin/apk-info-save-to-logs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # - - - - - - = = = - - - - - - . 
-echo "# > mkdir -pv ~/.logs/ ; apk info -v > ~/.log/apk_info_v.\$(date +d%y%m%dt%H%M%Sz%Z).log.txt "
-          mkdir -pv ~/.logs/ ; apk info -v > ~/.log/apk_info_v.$(date +d%y%m%dt%H%M%Sz%Z).log.txt 
+echo "# > mkdir -pv ~/.log/ ; apk info -v > ~/.log/apk_info_v.\$(date +d%y%m%dt%H%M%Sz%Z).log.txt "
+          mkdir -pv ~/.log/ ; apk info -v > ~/.log/apk_info_v.$(date +d%y%m%dt%H%M%Sz%Z).log.txt 
 # - - - - - - = = = - - - - - - . 
 #-eof
 
--- bin/dpkg-l-save-to-logs.sh
+++ bin/dpkg-l-save-to-logs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # - - - - - - = = = - - - - - - . 
-echo "# > mkdir -pv ~/.logs/ ; dpkg -l > ~/.log/dpkg_l.\$(date +d%y%m%dt%H%M%Sz%Z).log.txt "
-          mkdir -pv ~/.logs/ ; dpkg -l > ~/.log/dpkg_l.$(date +d%y%m%dt%H%M%Sz%Z).log.txt 
+echo "# > mkdir -pv ~/.log/ ; dpkg -l > ~/.log/dpkg_l.\$(date +d%y%m%dt%H%M%Sz%Z).log.txt "
+          mkdir -pv ~/.log/ ; dpkg -l > ~/.log/dpkg_l.$(date +d%y%m%dt%H%M%Sz%Z).log.txt 
 # - - - - - - = = = - - - - - - . 
 #-eof
 
